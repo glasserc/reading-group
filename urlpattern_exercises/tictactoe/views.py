0a13a479939bd0b27af3a6f36fde700d8ec7c045
Ethan Glasser-Camp <ethan@betacantrips.com>
2015-06-27 17:32:13 -0400

    add exercises

--- urlpattern_exercises/tictactoe/views.py
+++ urlpattern_exercises/tictactoe/views.py
@@ -9,3 +9,11 @@ def index(request):
 def game(request, game_id):
     """show just the game identified by the ID"""
     return HttpResponse('game {}'.format(game_id))
+
+def games_for_me(request):
+    """show the games for the current user"""
+    return HttpResponse('My open games')
+
+def games_for_player(request, player_name):
+    """show the games that a player is playing in"""
+    return HttpResponse('Games for {}'.format(player_name))
--- urlpattern_exercises/urlpattern_exercises/urls.py
+++ urlpattern_exercises/urlpattern_exercises/urls.py
@@ -2,9 +2,23 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'urlpattern_exercises.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    # Main entry point for Games People Play site.
+    # At present we only have one game, plus a news feed and a polls
+    # application that Freddy found online. We're going to add more
+    # games later.
+    #
+    # This is for the tic-tac-toe part of the site.
+    url(r'play', include('tictactoe.urls')),
 
+    # Original name for the blog was "The logs"
+    url(r'^logger/', include('blog.urls')),
+    # Our SEO consultant recommended that our URLs have "thoughts" in
+    # the name instead
+    url(r'^thoughts', include('blog.urls')),
+
+    # Polls
+    url(r'^voting/', include('polls.urls')),
+
+    # Of course, administrative functions are in the usual place
     url(r'^admin/', include(admin.site.urls)),
 ]
