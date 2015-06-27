6ad9ccc3889a3d6978131c2f074d42a07405df6e
Ethan Glasser-Camp <ethan@betacantrips.com>
2015-06-27 16:09:56 -0400

    add some views

--- urlpattern_exercises/polls/views.py
+++ urlpattern_exercises/polls/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    """show the most recent polls"""
+    return HttpResponse('Most recent polls')
+
+def poll(request, poll_id):
+    """show just the poll identified by the ID"""
+    return HttpResponse('poll {}'.format(poll_id))
--- urlpattern_exercises/tictactoe/views.py
+++ urlpattern_exercises/tictactoe/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    """show the most recent games"""
+    return HttpResponse('Most recent games')
+
+def game(request, game_id):
+    """show just the game identified by the ID"""
+    return HttpResponse('game {}'.format(game_id))
